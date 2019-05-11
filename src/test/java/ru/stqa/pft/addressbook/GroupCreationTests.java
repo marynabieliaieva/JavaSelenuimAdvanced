3032e9bd229f803ceecf97e4f05b66bbe6a48e15
marynelko <marynelko@gmail.com>
2019-05-11 19:39:05 +0200

    Add contact create test

--- src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,30 +1,58 @@
 package ru.stqa.pft.addressbook;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTests {
   FirefoxDriver wd;
 
+
   @BeforeMethod
   public void setUp() throws Exception{
     wd= new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+    wd.get("https://mailbook.nl/");
+    wd.findElement(By.linkText("Login")).click();
+    wd.findElement(By.id("email")).click();
+    wd.findElement(By.id("email")).clear();
+    wd.findElement(By.id("email")).sendKeys("marynelko@gmail.com");
+    wd.findElement(By.id("password")).click();
+    wd.findElement(By.id("password")).clear();
+    wd.findElement(By.id("password")).sendKeys("Ab123456");
+    wd.findElement(By.cssSelector("div[class='field row end']>button")).click();
   }
 
   @Test
-  public void GroupCreationTests(){
-    wd.get("https://www.postable.com");
-    wd.findElement(By.linkText("Login")).click();
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("marynelko@gmail.com");
-    wd.findElement(By.name("password")).click();
-    wd.findElement(By.name("password")).clear();
-    wd.findElement(By.name("password")).sendKeys("123456");
-    wd.findElement(By.cssSelector("button[class='full-width']")).click();
+  public void testGroupCreation(){
+    wd.findElement(By.cssSelector("div[class='field row addressbook']>button")).click();
+    wd.findElement(By.id("to")).click();
+    wd.findElement(By.id("to")).clear();
+    wd.findElement(By.id("to")).sendKeys("Lars");
+    wd.findElement(By.id("lastName")).click();
+    wd.findElement(By.id("lastName")).clear();
+    wd.findElement(By.id("lastName")).sendKeys("Jochansen");
+    wd.findElement(By.cssSelector("div[class='field row end']>button")).click();
+  }
 
+  @AfterMethod
+  public void tearDown(){
+    wd.quit();
+  }
+
+  public static boolean isAlertPresent(FirefoxDriver wd){
+    try{
+      wd.switchTo().alert();
+      return true;
+    }
+    catch (NoAlertPresentException e){
+      return false;
+      }
   }
 
 }
