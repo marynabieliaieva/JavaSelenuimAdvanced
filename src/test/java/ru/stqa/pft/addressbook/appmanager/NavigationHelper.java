041883189151338d87d462003b0d519252b6bdc0
marynelko <marynelko@gmail.com>
2019-06-19 12:15:47 +0200

    added conditions

--- src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -11,6 +12,10 @@ public class NavigationHelper extends HelperBase{
 
   public void goToAddContactPage() throws InterruptedException {
     Thread.sleep(1000);
+    By buttonLocator = By.cssSelector("span.button-group:nth-child(4) > button:nth-child(1)");
+    if (! isElementPresent(buttonLocator)&& wd.findElement(buttonLocator).getText().equals("Přidat kontakt")){
+      return;
+    }
     wd.get("https://email.seznam.cz/#abook");
   }
 }
