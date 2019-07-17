07b0632c003319398090f19961c6574ed50db27b
marynelko <marynelko@gmail.com>
2019-07-17 15:33:36 +0200

    data provider for data inside of the test

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,20 +1,32 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{"test 1"});
+    list.add(new Object[]{"test 2"});
+    list.add(new Object[]{"test 3"});
+    return list.iterator();
+  }
 
-
-  @Test
-  public void testGroupCreation() throws InterruptedException {
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(String name) throws InterruptedException {
     app.goTo().contactPage();
     Groups before = app.getGroupHelper().all(); //list of the elements
-    GroupData group = new GroupData().withName("Work");
+    GroupData group = new GroupData().withName(name);
     app.getGroupHelper().createGroup(group);
     app.goTo().contactPage();
     assertThat(app.getGroupHelper().getGroupCount(), equalTo(before.size() + 1));
