c37d81cf20ae67af292d33ff9b2b8d920df191f5
marynelko <marynelko@gmail.com>
2019-07-19 11:48:09 +0200

    orm database connection

--- src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,12 +4,25 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "group_list")
 
 @XStreamAlias("group")
 public class GroupData {
   @XStreamOmitField
+  @Id
+  @Column(name= "group_id")
   private int id = Integer.MAX_VALUE; // because of that group will be at the last place;
   @Expose
+  @Column(name= "group_name")
+  @Type(type="text")
   private String groupName;
 
   @Override
